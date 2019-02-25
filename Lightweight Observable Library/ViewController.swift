ee84e3065c3e5a587fe75c6a9c0906ec7c6455ee
Ryan Reynolds <4009911+ryanrey@users.noreply.github.com>
2019-02-24 22:36:01 -0500

    Revert UIViewController.swift file changes. Remove TestVC.swift

--- Lightweight Observable Library/ViewController.swift
+++ Lightweight Observable Library/ViewController.swift
@@ -10,50 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var name = Observable<String>.just("VC Initial value")
-    
     @IBOutlet weak var label: UILabel!
     
     @IBAction func goToNext(_ sender: Any) {
-        let testVC = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "TestVC") as! TestVC
-        
-        navigationController?.pushViewController(testVC, animated: true)
+        // TODO
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        setupSubscriptions()
-    }
-    
-    deinit {
-         print("ViewController deinit")
-    }
-    
-    private func setupSubscriptions() {
-        name.debug().subscribe(
-            onNext: { value in
-                print("ViewController onNext: \(value)")
-        },
-            onError: { error in
-                print("ViewController onError: \(error)")
-        },
-            onCompleted: {
-                print("ViewController onCompleted")
-        }).addToDisposeBag(disposeBag)
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        setupSubscriptions()
-       // name.value = "ViewController appeared"
-    }
-    
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        
-       // name.value = "ViewController disappeared"
     }
 }
 
